5c3df6935e0a1125dd02f95790f2ff3561634903
Dvorf <dvorfin@yandex.ru>
2023-07-12 17:23:26 +0300

    2 commit

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for
+from config import Config
+from project.forms import MessageForm
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def index():
-    return render_template('index.html')
+    server_message = ''
+    client_message = ''
+    if request.method == 'POST':
+        client_message = request.form.get('message')
+
+    if client_message == 'hi':
+        server_message = 'Hello'
+    elif client_message != '':
+        server_message = 'How are u?'
+
+    return render_template('index.html', message=server_message)
 
 
 @app.route('/contact/')
@@ -30,6 +44,22 @@ def calc(num1, operator, num2):
          '*': lambda x, y: x*y, ':': lambda x, y: x/y,
          '**': lambda x, y: x**y}
     return str(d[operator](num1, num2))
+
+
+@app.route('/message/', methods=['get', 'post'])
+def message():
+    form = MessageForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        print(name)
+        print(email)
+        print(message)
+        print('\nData received. Now redirecting...')
+        return redirect(url_for('message'))
+
+    return render_template('message.html', form=form)
 
 
 if __name__ == '__main__':
